8fa29bd08ed9524fc27efe8cc4007721e2b82a42
D34d M8 <spiderierusalim@mail.bg>
2020-11-02 20:16:27 +0300

    Changed formatting from single to double quotes

--- sp/sp.py
+++ sp/sp.py
@@ -306,7 +306,7 @@ class SpREPL:
 
 class SpSearcher:
     search_url = "https://www.startpage.com/do/search"
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+    user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"}
 
     def __init__(self, page_size):
         self.page_size = page_size
